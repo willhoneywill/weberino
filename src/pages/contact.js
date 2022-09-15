5a0ae3af85482c27f726db2934f938587ae3281d
William Honeywill <williamhoneywill@williams-mbp.speedport_w_724v_09011603_06_010>
2022-09-15 09:43:04 +0200

    added TW

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="Contact Page">
+            <p>This is the contact page</p>
+        </Layout>
+    )
+}
+
+export const Head = () => <title>Web Developer - about</title>
+
+export default AboutPage
